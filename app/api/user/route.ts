44b3bb8beb42f66824b5456d3e1eff6704fbdafe
Kevon Kibochi <devokrann@gmail.com>
2024-11-03 04:55:00 +0300

    Consolidate user record update logic

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -5,6 +5,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 import { compareHashes, hashValue } from "@/utilities/helpers/hasher";
 import { UserCreate, UserGet, UserUpdate } from "@/types/models/user";
+import { OtlType } from "@prisma/client";
+import { generateToken } from "@/utilities/generators/token";
+import { baseUrl } from "@/data/constants";
+import { emailCreatePasswordForgot } from "@/libraries/wrappers/email/send/auth/password";
 
 export async function POST(request: NextRequest) {
 	try {
@@ -19,7 +23,7 @@ export async function POST(request: NextRequest) {
 		await prisma.user.create({
 			data: {
 				id: generateId(),
-				name: user.name || null,
+				name: user.name,
 				email: user.email,
 				profile: !user.name
 					? undefined
@@ -48,13 +52,17 @@ export async function PUT(request: NextRequest) {
 			return NextResponse.json({ error: "You must be signed in" }, { status: 401, statusText: "Unauthorized" });
 		}
 
-		const { user, options }: { user: UserUpdate; options: { name: boolean; password: boolean } } =
+		const { user, options }: { user: UserUpdate; options: { name: boolean; password: "forgot" | "update" } } =
 			await request.json();
 
 		const userRecord = await prisma.user.findUnique({ where: { email: user.email as string } });
 
 		if (!userRecord) {
 			return NextResponse.json({ error: "User not found" }, { status: 404, statusText: "Not Found" });
+		}
+
+		if (!userRecord.verified) {
+			return NextResponse.json({ error: "User not verified" }, { status: 403, statusText: "Not Veried" });
 		}
 
 		if (options.name) {
@@ -77,7 +85,7 @@ export async function PUT(request: NextRequest) {
 			);
 		}
 
-		if (options.password) {
+		if (options.password == "update") {
 			const passwordMatch =
 				(!user.password && !userRecord.password) ||
 				(await compareHashes(user.password as string, userRecord.password));
@@ -96,6 +104,67 @@ export async function PUT(request: NextRequest) {
 			return NextResponse.json(
 				{ message: "Password changed successfully" },
 				{ status: 200, statusText: "Password Changed" }
+			);
+		}
+
+		if (options.password == "forgot") {
+			const otlRecord = await prisma.otl.findUnique({
+				where: { userId_type: { userId: userRecord.id, type: OtlType.PASSWORD_RESET } },
+			});
+
+			const now = new Date();
+			const expired = otlRecord?.expiresAt! < now;
+
+			if (otlRecord && !expired) {
+				const expiry = otlRecord.expiresAt.getTime() - now.getTime();
+
+				return NextResponse.json(
+					{ error: "OTL already sent", expiry },
+					{ status: 409, statusText: "Already Sent" }
+				);
+			}
+
+			otlRecord &&
+				expired &&
+				(await prisma.otl.delete({
+					where: { userId_type: { userId: userRecord.id, type: OtlType.PASSWORD_RESET } },
+				}));
+
+			const token = await generateToken(
+				{
+					id: userRecord.id,
+					email: userRecord.email,
+					password: userRecord.password || process.env.AUTH_SECRET!,
+				},
+				5
+			);
+
+			const otlValue = `${baseUrl}/auth/password/reset/${userRecord.id}/${token}`;
+
+			const otlHash = await hashValue(otlValue);
+
+			await prisma.user.update({
+				where: { email: userRecord.email },
+				data: {
+					otls: {
+						create: [
+							{
+								id: generateId(),
+								type: OtlType.PASSWORD_RESET,
+								otl: otlHash!,
+								expiresAt: new Date(Date.now() + 60 * 60 * 1000),
+							},
+						],
+					},
+				},
+			});
+
+			return NextResponse.json(
+				{
+					message: "An OTL has been sent",
+					resend: await emailCreatePasswordForgot(otlValue, userRecord.email),
+				},
+				{ status: 200, statusText: "OTL Sent" }
 			);
 		}
 	} catch (error) {
