b8e618cddb5e981882ae266a8a086d8eff323f8c
Kevon Kibochi <devokrann@gmail.com>
2024-11-30 15:54:48 +0300

    Add home page alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,18 @@ import NavbarMain from '@/components/layout/navbars/main';
 import FooterMain from '@/components/layout/footers/main';
 import LayoutSection from '@/components/layout/section';
 import CardCtaNewsletter from '@/components/common/cards/cta/newsletter';
+import LayoutBarMain from '@/components/layout/bars/main';
 
 import AffixTop from '@/components/common/affixi/top';
+import AffixNavbar from '@/components/common/affixi/navbar';
 
 export default function Home() {
   return (
-    <LayoutBody nav={<NavbarMain />} footer={<FooterMain />}>
+    <LayoutBody
+      bar={<LayoutBarMain />}
+      nav={<NavbarMain />}
+      footer={<FooterMain />}
+    >
       <main>
         <LayoutSection id={'page-home'} margined>
           Home page
@@ -22,6 +28,7 @@ export default function Home() {
       </main>
 
       <AffixTop />
+      <AffixNavbar />
     </LayoutBody>
   );
 }
