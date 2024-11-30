bedf90d06a3dd222b5794f12a9c080bb1ea878e9
Kevon Kibochi <devokrann@gmail.com>
2024-11-30 12:07:04 +0300

    Setup onboarding email (#73)
    * Setup onboarding email
    
    * Fix email verification bug (#72)

--- src/libraries/wrappers/email/transactional/auth/password.ts
+++ src/libraries/wrappers/email/transactional/auth/password.ts
@@ -1,41 +1,14 @@
 import appData from '@/data/app';
 import resend from '@/libraries/resend';
 
-import EmailTransactionalAuthVerify from '@/components/email/transactional/auth/verify';
+import EmailTransactionalAuthPasswordForgot from '@/components/email/transactional/auth/password-forgot';
+import EmailTransactionalAuthPasswordChanged from '@/components/email/transactional/auth/password-changed';
 import { isProduction } from '@/utilities/helpers/environment';
 import { EmailInquiry } from '@/types/email';
 import { render } from '@react-email/render';
-import EmailTransactionalOnboardNewsletter from '@/components/email/transactional/onboard/newsletter';
 
-export const emailSendAuthEmailVerify = async (
-  otp: string,
-  options: { to: EmailInquiry['to']; signUp?: boolean }
-) => {
-  const { data, error } = await resend.general.emails.send({
-    from: `${appData.name.app} <${
-      isProduction()
-        ? process.env.NEXT_EMAIL_NOREPLY!
-        : process.env.NEXT_RESEND_EMAIL!
-    }>`,
-    to: [isProduction() ? options.to : process.env.NEXT_EMAIL_INFO!],
-    subject: `Verify Your Email Address`,
-    html: await render(
-      EmailTransactionalAuthVerify({
-        otp,
-        options: { signUp: options.signUp ?? true },
-      })
-    ),
-    replyTo: process.env.NEXT_EMAIL_NOREPLY!,
-  });
-  if (!error) {
-    return data;
-  } else {
-    console.error('---> wrapper error - (email send sign up):', error);
-    throw error;
-  }
-};
-
-export const emailSendAuthEmailChanged = async (
+export const sendEmailTransactionalAuthPasswordForgot = async (
+  otl: string,
   options: EmailInquiry['to']
 ) => {
   const { data, error } = await resend.general.emails.send({
@@ -45,15 +18,41 @@ export const emailSendAuthEmailChanged = async (
         : process.env.NEXT_RESEND_EMAIL!
     }>`,
     to: [isProduction() ? options : process.env.NEXT_EMAIL_NOREPLY!],
-    subject: `Email Changed`,
-    html: await render(EmailTransactionalOnboardNewsletter()),
+    subject: 'Reset Your Password',
+    html: await render(EmailTransactionalAuthPasswordForgot({ otl })),
     replyTo: process.env.NEXT_EMAIL_NOREPLY!,
   });
   if (!error) {
     return data;
   } else {
     console.error(
-      '---> wrapper error - (email create (send) password changed):',
+      '---> wrapper error - (send email (forgot password)):',
+      error
+    );
+
+    throw error;
+  }
+};
+
+export const sendEmailTransactionalAuthPasswordChanged = async (
+  options: EmailInquiry['to']
+) => {
+  const { data, error } = await resend.general.emails.send({
+    from: `${appData.name.app} <${
+      isProduction()
+        ? process.env.NEXT_EMAIL_NOREPLY!
+        : process.env.NEXT_RESEND_EMAIL!
+    }>`,
+    to: [isProduction() ? options : process.env.NEXT_EMAIL_NOREPLY!],
+    subject: `Password Changed`,
+    html: await render(EmailTransactionalAuthPasswordChanged()),
+    replyTo: process.env.NEXT_EMAIL_NOREPLY!,
+  });
+  if (!error) {
+    return data;
+  } else {
+    console.error(
+      '---> wrapper error - send email (password changed)):',
       error
     );
 
